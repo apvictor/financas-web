1023e70cd53d16fbbf06fedcf0902aa786a0bdd6
Armando Pereira <armando2019ti@gmail.com>
2024-01-04 16:41:28 -0300

    🐞 fix: corrigir tela home

--- src/view/pages/Home/components/CardCostCenter.tsx
+++ src/view/pages/Home/components/CardCostCenter.tsx
@@ -1,5 +1,5 @@
 import { formatCurrency } from "../../../../helpers/formatCurrency";
-import { GraduationCap, HomeIcon, LineChart, Pizza } from "lucide-react";
+import { IconCostCenter } from "../../../components/IconCostCenter";
 
 interface Props {
   title: string;
@@ -17,26 +17,7 @@ export function CardCostCenter({ title, limit, value, totalIncome }: Props) {
     <button className="bg-[#1C1E21] flex flex-col items-center gap-6 p-3 rounded-md">
       <div className="w-full flex items-center justify-between">
         <div className="flex items-center gap-2">
-          {title.toUpperCase() === "CASA" && (
-            <div className="p-1.5 bg-[#A5D8FF] rounded-full">
-              <HomeIcon color="#1C7ED6" size={16} />
-            </div>
-          )}
-          {title.toUpperCase() === "INVESTIMENTOS" && (
-            <div className="p-1.5 bg-[#FFE1E1] rounded-full">
-              <LineChart color="#FF6E6E" size={16} />
-            </div>
-          )}
-          {title.toUpperCase() === "EDUCAÇÃO" && (
-            <div className="p-1.5 bg-[#98FFED] rounded-full">
-              <GraduationCap color="#196D5D" size={16} />
-            </div>
-          )}
-          {title.toUpperCase() === "LAZER" && (
-            <div className="p-1.5 bg-[#FFE3C8] rounded-full">
-              <Pizza color="#FF7A00" size={16} />
-            </div>
-          )}
+          <IconCostCenter title={title} />
           <span className="text-sm text-[#AAA]">{title}</span>
         </div>
         <span
@@ -56,7 +37,6 @@ export function CardCostCenter({ title, limit, value, totalIncome }: Props) {
           className="bg-[#343A40] h-1 rounded-xl relative"
           style={{
             width: `100%`,
-
           }}
         >
           <div
