cc87780747cb23d6cf014ec062d64296b0e16c86
Armando Pereira <armando2019ti@gmail.com>
2024-03-15 14:37:27 -0300

    🐞 fix: corrigir estilos da lista de minhas contas e meus centros de custos

--- src/view/pages/Home/components/CardAccount.tsx
+++ src/view/pages/Home/components/CardAccount.tsx
@@ -1,4 +1,3 @@
-import { Line } from "../../../components/Line";
 import { getBank } from "../../../../app/helpers/bank";
 import { AccountModel } from "../../../../app/models/AccountModel";
 import { VisibilityValue } from "../../../components/VisibilityValue";
@@ -9,25 +8,21 @@ interface Props {
 }
 export function CardAccount({ account, openModalAccountEdit }: Props) {
   return (
-    <div className="flex flex-col gap-4 mt-2">
-      <Line />
-
-      <button
-        className="flex items-center justify-between w-full"
-        onClick={openModalAccountEdit}
-      >
-        <div className="flex items-center gap-2">
-          <img
-            alt={account.name}
-            src={getBank(account.name)}
-            className="h-9 w-9 rounded-full"
-          />
-          <span className="text-xs">{account.name}</span>
-        </div>
-        <span className="font-bold text-sm">
-          <VisibilityValue value={account.incomeTotal - account.expenseTotal} />
-        </span>
-      </button>
-    </div>
+    <button
+      className="flex items-center justify-between w-full p-4 mt-2 rounded-lg bg-slate-900"
+      onClick={openModalAccountEdit}
+    >
+      <div className="flex items-center gap-2">
+        <img
+          alt={account.name}
+          src={getBank(account.name)}
+          className="h-8 w-8 rounded-full"
+        />
+        <span className="text-xs">{account.name}</span>
+      </div>
+      <span className="font-bold text-sm">
+        <VisibilityValue value={account.incomeTotal - account.expenseTotal} />
+      </span>
+    </button>
   );
 }
--- src/view/pages/Home/components/CardCostCenter.tsx
+++ src/view/pages/Home/components/CardCostCenter.tsx
@@ -1,4 +1,3 @@
-import { Line } from "../../../components/Line";
 import { IconCostCenter } from "../../../components/IconCostCenter";
 import { VisibilityValue } from "../../../components/VisibilityValue";
 
@@ -22,64 +21,60 @@ export function CardCostCenter({
   const porcentagem = (valor_gasto / valor_limite_gasto) * 100;
 
   return (
-    <div className="flex flex-col gap-4 mt-2">
-      <Line />
-
-      <button
-        className="flex flex-col items-center justify-between gap-4"
-        onClick={openModalCostCenterEdit}
-      >
-        <div className="w-full flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <IconCostCenter title={title} />
-            <span className="text-sm text-white">{title}</span>
-          </div>
-          <span
-            className={`text-xs
-          ${limite_excedido ? "text-expense-900" : "text-white"}`}
-          >
-            {isNaN(porcentagem) ? 0 : porcentagem.toFixed(0)}%
-          </span>
+    <button
+      className="flex flex-col items-center justify-between gap-4 p-4 mt-2 rounded-lg bg-slate-900"
+      onClick={openModalCostCenterEdit}
+    >
+      <div className="w-full flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <IconCostCenter title={title} />
+          <span className="text-sm text-white">{title}</span>
         </div>
-        <div className="flex flex-col w-full gap-4">
-          <div className="bg-gray-900 h-2 rounded-xl relative w-full">
-            <div
-              className={`h-2 rounded-xl relative
+        <span
+          className={`text-xs
+          ${limite_excedido ? "text-expense-900" : "text-white"}`}
+        >
+          {isNaN(porcentagem) ? 0 : porcentagem.toFixed(0)}%
+        </span>
+      </div>
+      <div className="flex flex-col w-full gap-4">
+        <div className="bg-gray-800 h-2 rounded-xl relative w-full">
+          <div
+            className={`h-2 rounded-xl relative
               ${limite_excedido ? "bg-expense-900" : "bg-primary"}`}
-              style={{
-                width: `${
-                  limite_excedido
-                    ? 100
-                    : isNaN(porcentagem)
-                    ? 0
-                    : porcentagem.toFixed(0)
-                }%`,
-              }}
-            >
-              {porcentagem > 18 && (
-                <span
-                  className={`absolute right-0 bottom-[-6px] text-[10px] rounded-lg p-0.5 bg-gray-800 border
+            style={{
+              width: `${
+                limite_excedido
+                  ? 100
+                  : isNaN(porcentagem)
+                  ? 0
+                  : porcentagem.toFixed(0)
+              }%`,
+            }}
+          >
+            {porcentagem > 18 && (
+              <span
+                className={`absolute right-0 bottom-[-6px] text-[10px] rounded-lg p-0.5 bg-gray-800 border
                   ${limite_excedido ? "border-expense-900" : "border-primary"}
                   `}
-                >
-                  <VisibilityValue value={valor_gasto} />
-                </span>
-              )}
-            </div>
-          </div>
-          <div className="flex justify-between items-center w-full text-xs">
-            <span
-              className={`flex justify-between items-center w-full text-xs
-            ${limite_excedido ? "text-expense-900" : "text-white"}`}
-            >
-              <VisibilityValue value={valor_gasto} />
-            </span>
-            <span className="text-white">
-              <VisibilityValue value={valor_limite_gasto} />
-            </span>
+              >
+                <VisibilityValue value={valor_gasto} />
+              </span>
+            )}
           </div>
         </div>
-      </button>
-    </div>
+        <div className="flex justify-between items-center w-full text-xs">
+          <span
+            className={`flex justify-between items-center w-full text-xs
+            ${limite_excedido ? "text-expense-900" : "text-white"}`}
+          >
+            <VisibilityValue value={valor_gasto} />
+          </span>
+          <span className="text-white">
+            <VisibilityValue value={valor_limite_gasto} />
+          </span>
+        </div>
+      </div>
+    </button>
   );
 }
